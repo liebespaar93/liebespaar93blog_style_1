328fa807f78c84de54cea1746ec835c127f7ec65
아기새 <82822684+liebespaar93@users.noreply.github.com>
2023-10-14 18:21:01 +0900

    57 메인페이지 만들기 (#58)
    * 🎨 Design : 디자인 작업
    
    - [x] ✅ Done : 배너만들기
    
    * 🎨 Design : 베너 작업
    
    - [x] ✅ Done : 베너 이미지 반응형
    - [x] ✅ Done : 베너 svg neon 스타일 ani 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 import Other from './other/page'
 import Nav from './components/Nav'
@@ -6,8 +5,32 @@ import Nav from './components/Nav'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Nav />
-      안녕하세요
+      <div className={styles.contanier}>
+        <div className={styles.benner}>
+          <div className={styles.benner_img}>
+            <img src="/images/blog_benner.gif" data-speedx="0.3" loading='lazy' alt="blog_benner" />
+          </div>
+        </div>
+        <div className={styles.home}>
+          <div className={styles.title}>
+            <div className={styles.svg} >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none">
+                <text x="50%" y="50%" text-anchor="middle">
+                  Blog
+                </text>
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none">
+                <text x="50%" y="50%" text-anchor="middle">
+                  Neon Light
+                </text>
+              </svg>
+            </div>
+          </div>
+          <div className={styles.context}>
+            testestse
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
