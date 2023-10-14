d9ec3ccff7a4f880c08c05fb4bbaa596c0b84108
아기새 <82822684+liebespaar93@users.noreply.github.com>
2023-10-15 02:24:21 +0900

    🌑 Github : 깃허브 환경 작업
    - [x] ✅ Done : Template 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,44 @@
+import { GithubFaceLine } from '@/components/assests/Github'
 import styles from './page.module.css'
-import Other from './other/page'
-import Nav from './components/Nav'
+import parallax from '@/styles/parallax.module.css'
 
 export default function Home() {
+
   return (
-    <main className={styles.main}>
-      <div className={styles.contanier}>
-        <div className={styles.benner}>
+    <main className={`${styles.main} ${parallax.parallax}`}>
+      <div className={`${styles.contanier} ${parallax.parallax_group}`}>
+        <div className={`${styles.benner} ${parallax.parallax_layer_back}`}>
           <div className={styles.benner_img}>
             <img src="/images/blog_benner.gif" data-speedx="0.3" loading='lazy' alt="blog_benner" />
           </div>
         </div>
-        <div className={styles.home}>
+        <div className={`${styles.home} ${parallax.parallax_layer_base}`}>
           <div className={styles.title}>
             <div className={styles.svg} >
               <svg xmlns="http://www.w3.org/2000/svg" fill="none">
-                <text x="50%" y="50%" text-anchor="middle">
+                <text x="50%" y="50%" textAnchor="middle">
                   Blog
                 </text>
               </svg>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none">
-                <text x="50%" y="50%" text-anchor="middle">
+                <text x="50%" y="50%" textAnchor="middle">
                   Neon Light
                 </text>
               </svg>
             </div>
           </div>
           <div className={styles.context}>
-            testestse
+            <div className={styles.git_info}>
+              <div className={styles.git_svg}>
+                <GithubFaceLine />
+              </div>
+              <div>
+                <ul>
+                  <li>aaa</li>
+                  <li>ss</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
